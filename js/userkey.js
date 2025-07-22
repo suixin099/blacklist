1d45146858572bb466ef0968dc1b95836874f2c7
shaw99 <1820894104@qq.com>
2025-07-22 22:19:45 +0800

    update
    Signed-off-by: shaw99 <1820894104@qq.com>

--- js/userkey.js
+++ js/userkey.js
@@ -9,6 +9,7 @@ const initGatekeeper = () => {
     { data: "Mzc4MTM4OTY1fGFpbmkxMzE0" }, //*|*
 	{ data: "bGNjMjAyMXwyNzE5ODk=" }, //2465932040 木子
 	{ data: "YW41MjB8YW41MjA=" }, //硬酱
+	{ data: "cWF6d3N4fDEyMzQ1Njc4OTA=" }, //清澄
   ];
   
   const getValidCredentials = () => {
