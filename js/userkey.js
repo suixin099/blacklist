a91fc0ed56b7929779497dcc2984ed6101c93a00
shaw99 <1820894104@qq.com>
2026-03-17 15:04:41 +0800

    update

--- js/userkey.js
+++ js/userkey.js
@@ -5,7 +5,7 @@ const initGatekeeper = () => {
   const decrypt = (encodedStr) => atob(encodedStr);
 
   const encryptedCredentials = [
-	//{ data: "c2hlbnJ1fHRvdWxhb2Jhbg==" }, //共用
+	{ data: "c2hlbnJ1fHRvdWxhb2Jhbg==" }, //共用
     { data: "Mzc4MTM4OTY1fGFpbmkxMzE0" }, //*|*
 	{ data: "bGNjMjAyMXwyNzE5ODk=" }, //2465932040 木子
 	{ data: "YW41MjB8YW41MjA=" }, //硬酱
