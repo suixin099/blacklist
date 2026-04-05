0d6587ff299540c36ceb5a0f0dd64da807dde7a9
shaw99 <1820894104@qq.com>
2026-04-06 00:05:37 +0800

    update

--- js/userkey.js
+++ js/userkey.js
@@ -24,6 +24,7 @@ const initGatekeeper = () => {
 	{ data: "cWF6MzM1fHFxMTIzNDU2" }, //3676171200 伽澜
 	{ data: "aGVpaGVpODg4fGExMjM0NTY=" }, //1095824511 黑黑
 	{ data: "ZHVvZHVvfDM3MzgzOWFi" }, //3440932924 囡的小迷妹
+	{ data: "bGxsMTIzMTIzfGxsbDEyMzEyMw==" }, //479605794 上瘾
   ];
   
   const getValidCredentials = () => {
